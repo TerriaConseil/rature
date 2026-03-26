fe50d989e5c971d6589f1cae8d61f0f9f1d251d1
Julien Kilo <4519042+alphabeat@users.noreply.github.com>
2026-03-26 09:20:13 +0100

    Set English as fallback lang

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import fr from '../locales/fr/translation.json' with { type: 'json' };
-import en from '../locales/en/translation.json' with { type: 'json' };
+import fr from '@/locales/fr/translation.json' with { type: 'json' };
+import en from '@/locales/en/translation.json' with { type: 'json' };
 
 i18n
   .use(LanguageDetector)
@@ -13,7 +13,7 @@ i18n
       fr: { translation: fr },
       en: { translation: en },
     },
-    fallbackLng: 'fr',
+    fallbackLng: 'en',
     supportedLngs: ['fr', 'en'],
     interpolation: {
       escapeValue: false,
