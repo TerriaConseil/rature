5360015ec7b179abb2f47fc9dd09257b340378bf
Julien Kilo <4519042+alphabeat@users.noreply.github.com>
2026-03-28 15:02:30 +0100

    Ignored labels are not needed anymore

--- src/models/camembert-ner-pii.ts
+++ src/models/camembert-ner-pii.ts
@@ -5,7 +5,6 @@ export class CamembertNerPiiModel extends TokenClassificationModel {
     super('Anonym-IA/V2-camembert-ner-pii-onnx-fp32', {
       aggregationStrategy: 'none',
       partOfWordStrategy: 'bio-tagging',
-      ignoredLabels: ['B-DATE', 'I-DATE', 'B-URL', 'I-URL', 'B-IBAN', 'I-IBAN', 'B-EMAIL', 'I-EMAIL'],
     });
   }
 }
